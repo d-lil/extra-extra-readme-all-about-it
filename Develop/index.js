44ce53c13de9561682c8041b5be40ebdca04b884
Danny <dahneel@gmail.com>
2023-02-26 19:24:27 -0700

    Added the rest of the code lol, probably should have pushed more a few hours ago

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,78 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const { writeFile } = require('fs').promises;
+const generateMarkdown = require('./utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
-const questions = [];
-
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+const questions = () => {
+    return inquirer.prompt([  
+        {
+            type: "input",
+            name: "title",
+            message: "What is the title of your project?"
+        },
+        {
+            type: "input",
+            name: "author",
+            message: "What is the author's name?"
+        },
+        {
+            type: "input",
+            name: "username",
+            message: "What your GitHub username?"
+            },
+        {
+            type: "input",
+            name: "email",
+            message: "What is your email?"
+        }, 
+        {
+            type: "input",
+            name: "year",
+            message: "Please enter current year to add to license:"
+        },
+        {
+            type: "input",
+            name: "description",
+            message: "Please write a description of your project:"
+            },
+       
+        {
+            type: "input",
+            name: "installation",
+            message: "What (if any) command should be run to install dependencies?"
+        }, 
+        {
+            type: "input",
+            name: "usage",
+            message: "What is necessary for the user to know about the usage of the app/repo?"
+            }, 
+        {
+            type: "list",
+            name: "license",
+            message: "What type of license would you like for your project?",
+            choices: ["MIT","Apache 2.0","BSD 3","GNU GPL v3.0","None"]
+        },
+        {
+            type: "input",
+            name: "contribute",
+            message: "How can the user contribute to the project?"
+            },
+        {
+            type: "input",
+            name: "tests",
+            message: "What command should be entered to run tests? (if any)"
+            },
+    ])
+};
 
 // TODO: Create a function to initialize app
-function init() {}
+const init = () => {
+    questions()
+    .then((answers) => writeFile("README.md", generateMarkdown(answers)))
+    .then(() => console.log("Successfully wrote to README.md"))
+    .catch((err) => console.error(err));
+};
 
 // Function call to initialize app
 init();
--- README.md
+++ README.md
@@ -1,2 +1,39 @@
-# extra-extra-readme-all-about-it
-README gen from command-line
+# EXTRA-EXTRA-README-ALL-ABOUT-IT
+  [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+  This is a README generator created in command-prompt
+  ## Table of Contents:
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [License](#license)
+  * [Contributing](#contributing)
+  * [Tests](#tests)
+  * [Questions](#questions)
+  ## Installation:
+  To install the dependencies needed, open the console and run:
+  npm i
+  ## Usage:
+
+  Open the index.js file in integrated terminal and follow the prompts to create a README file for your project (I have even created this README using the gen ;p )
+  - You will still need to add your URL and Screenshots to README
+
+  **Prompt example:**
+  * ![prompt example](./Develop/utils/images/commandREADME.png)
+  **Results:**
+  * ![results](./Develop/utils/images/READMEresults.png)
+
+  ## License:
+  The MIT License (MIT)
+
+      Copyright (c) undefined undefined
+      
+      Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+      
+      The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+      
+      THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+  ## Contributing:
+  Sorry, but you cannot.
+  ## Tests:
+  N/A
+  ## Questions:
+  If you have questions, you may contact me on [GitHub](https://github.com/d-lil) or you may reach out by contacting Danny Liljegren at dahneel@gmail.com
